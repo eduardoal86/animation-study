7324f12cad41676998f2c56ad5861ad27f54b76a
ealves <ealves@moobie.com.br>
2017-07-17 16:39:37 -0300

    Added new efect slide

--- app/src/main/java/edualves/com/coordinatorlayout/MainActivity.java
+++ app/src/main/java/edualves/com/coordinatorlayout/MainActivity.java
@@ -1,7 +1,9 @@
 package edualves.com.coordinatorlayout;
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -11,6 +13,7 @@ import android.widget.LinearLayout;
 public class MainActivity extends Activity {
 
     private LinearLayout container;
+    private LinearLayout target;
     private Button btn;
     private Button hideBtn;
     private Button slideReg;
@@ -22,6 +25,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         container = (LinearLayout) findViewById(R.id.container_parent);
+        target = (LinearLayout) findViewById(R.id.container_target);
+
         btn = (Button) findViewById(R.id.btn);
         slideReg = (Button) findViewById(R.id.btn_slide);
         slideDec = (Button) findViewById(R.id.btn_slide_dec);
@@ -41,6 +46,21 @@ public class MainActivity extends Activity {
                 container.setVisibility(View.VISIBLE);
                 final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_overshoot);
                 container.setAnimation(animation);
+
+                final ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 210);
+                valueAnimator.setDuration(600);
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+
+                        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) target.getLayoutParams();
+                        layoutParams.setMargins(0, (Integer) valueAnimator.getAnimatedValue(), 0, 0);
+                        target.setLayoutParams(layoutParams);
+                    }
+                });
+                valueAnimator.start();
+
+
             }
         });
 
@@ -66,6 +86,23 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 container.setVisibility(View.GONE);
+
+                final ValueAnimator valueAnimator = ValueAnimator.ofInt(210, 0);
+                valueAnimator.setDuration(600);
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+
+                        CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) target.getLayoutParams();
+                        layoutParams.setMargins(0, (Integer) valueAnimator.getAnimatedValue(), 0, 0);
+                        target.setLayoutParams(layoutParams);
+                    }
+                });
+                valueAnimator.start();
+
+                CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) target.getLayoutParams();
+                layoutParams.setMargins(0, 0, 0, 0);
+                target.setLayoutParams(layoutParams);
             }
         });
 
